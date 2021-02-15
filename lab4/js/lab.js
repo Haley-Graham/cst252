b1417d50b793431e3a3d625067eaa7067770682a
Haley-Graham <hagraham@csumb.edu>
2021-02-14 16:29:41 -0800

    lab 5

---
+++ lab4/js/lab.js
@@ -0,0 +1,26 @@
+//Wake up
+  //At around 8am
+//Scroll through social media applications
+  //Til about 8:30am
+//Go into bathroom; Wash face and brush teeth
+  //Change out of pajamas
+//Change into gym attire
+//Drink pre-workout energy drink
+  //Celsius: Watermelon
+//Go to the gym
+//Come home and make breakfast & iced coffee
+  //Avocado toast with an egg
+//Eat breakfast
+
+//Write HTML code in Atom
+//Save it
+//Copy full path
+//Paste into browser
+  //ensure it looks correct
+//Open to Github
+//Write a brief description
+  //Write title and type of file
+//Click “Commit to Master”
+//Click “Push Origin”
+//Wait for it to upload
+//Type in url to make sure it is uploaded and working
---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+// Author: Haley Graham
+// Created: 14 February
+// License: Public Domain
+
+// Define Variables
+Make= "Volkswagon";
+Model= "Jetta";
+Color= "red";
+Year= 2007;
+
+// Calculate
+age = 2021 - Year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
