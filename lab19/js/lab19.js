371b1e9d1781a84cbc2a77a694a5a42620f018b1
Haley-Graham <hagraham@csumb.edu>
2021-04-19 13:38:53 -0700

    lab 19

---
+++ lab19/js/lab19.js
@@ -0,0 +1,24 @@
+// Author: Haley Graham
+// Created: 19 April
+// License: Public Domain
+
+var endpoint = "http://numbersapi.com/random/year";
+// Add a click event to the button
+$("#activate").click(function(){
+	$("#output").append("<p>Howdy!</p>");
+// When a user clicks the button, use a jQuery AJAX call to fetch output from your API
+  $.ajax({
+      url: endpoint,
+      data: { },
+      type: "GET",
+  })
+  // Put the output of the AJAX call in the output div.
+  .done(function( data ) {
+      $("#output").append("<p>" + data + "</p>");
+  })
+  // If the request fails
+  .fail(function( xhr, status, errorThrown ) {
+      console.log(errorThrown + " Status:" + status );
+  });
+
+})
